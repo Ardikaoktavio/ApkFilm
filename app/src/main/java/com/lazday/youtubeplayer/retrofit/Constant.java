ee24d6e5700bf618cbef31d5942eef3078a28ffb
aris <aris@ariss-MacBook-Pro.local>
2022-06-14 14:32:20 +0700

    isi dengan api TMD

--- app/src/main/java/com/lazday/youtubeplayer/retrofit/Constant.java
+++ app/src/main/java/com/lazday/youtubeplayer/retrofit/Constant.java
@@ -2,7 +2,7 @@ package com.lazday.youtubeplayer.retrofit;
 
 public class Constant {
 
-    public static final String KEY              = "28958498126627abaac10776d018b0f5";
+    public static final String KEY              = "";  //isi dengan api themovieDB mu
     public static final String BASE_URL         = "https://api.themoviedb.org/3/";
     public static final String BACKDROP_PATH    = "https://image.tmdb.org/t/p/w500_and_h282_face";
     public static final String POSTER_PATH      = "https://image.tmdb.org/t/p/w220_and_h330_face";
